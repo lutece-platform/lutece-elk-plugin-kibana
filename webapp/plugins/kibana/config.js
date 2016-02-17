31a14ade1398d674e9760e5cae7b45d650273a01
solenne robert <solenne.robert@atos.net>
2016-02-17 17:36:19 +0100

    KIBANA-3: Kibana url

--- webapp/plugins/kibana/config.js
+++ webapp/plugins/kibana/config.js
@@ -29,7 +29,7 @@ function (Settings) {
      *  +elasticsearch: {server: "http://localhost:9200", withCredentials: true}+
      *
      */
-    elasticsearch: "https://dev.lutece.paris.fr/poc/dev/elasticsearch",
+    elasticsearch: "http://dev.lutece.paris.fr/poc/dev/elasticsearch",
 
     /** @scratch /configuration/config.js/5
      *
