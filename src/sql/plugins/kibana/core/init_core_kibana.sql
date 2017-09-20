c94df3e990d1dd76a78016ee631d3f33d842d354
Cyril Moire <cyril.moire@atos.net>
2017-09-20 12:26:20 +0200

    KIBANA-15 : Add role 'Kibana management' to super admin in SQL init script

--- src/sql/plugins/kibana/core/init_core_kibana.sql
+++ src/sql/plugins/kibana/core/init_core_kibana.sql
@@ -13,8 +13,8 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 --
 -- Data for table core_user_right
 --
-DELETE FROM core_user_right WHERE id_right = 'KIBANA_RBAC_MANAGEMENT';
-INSERT INTO core_user_right (id_right,id_user) VALUES ('KIBANA_RBAC_MANAGEMENT',1);
+DELETE FROM core_user_right WHERE id_right = 'KIBANA_MANAGEMENT';
+INSERT INTO core_user_right (id_right,id_user) VALUES ('KIBANA_MANAGEMENT',1);
 
 DELETE FROM core_user_right WHERE id_right = 'KIBANA_RBAC_MANAGEMENT';
 INSERT INTO core_user_right (id_right,id_user) VALUES ('KIBANA_RBAC_MANAGEMENT',1);
